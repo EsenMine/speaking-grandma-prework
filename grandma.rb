7470f2266195c612c9b434262165958ad06c3f42
Miné E <mine_esen@yahoo.ca>
2019-05-29 01:53:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts"I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase
